39ffc37ddbd7cfd0897d6bbdf6cb2272ae5ad509
babygrenade <babygrenade.erg@gmail.com>
2023-05-27 01:02:37 -0400

    fixed snapshot, fixed lookup to ergo watch, broke exclude contracts

--- js/ajaxUtils.js
+++ js/ajaxUtils.js
@@ -1,7 +1,7 @@
 function getListData() {
     $.ajax({
       type: "GET",
-      url: 'https://raw.githubusercontent.com/glasgowm148/ergo-tokenautics/master/token_list.csv',
+      url: 'https://raw.githubusercontent.com/babygrenade/ergo-tokenautics/master/token_list.csv',
       dataType: 'text',
       success: function(data) {
         getList(data);
@@ -12,7 +12,7 @@ function getListData() {
   function getCommitData() {
     $.ajax({
       type: "GET",
-      url: 'https://api.github.com/repos/glasgowm148/ergo-tokenautics/commits?per_page=100',
+      url: 'https://api.github.com/repos/babygrenade/ergo-tokenautics/commits?per_page=100',
       dataType: 'json',
       success: function(data) {
         getCommits(data);
@@ -23,7 +23,7 @@ function getListData() {
   function getChartData(selectedTokenId, selectedCommitId) {
     $.ajax({
         type: "GET",
-        url: 'https://raw.githubusercontent.com/glasgowm148/ergo-tokenautics/master/token_list.csv',
+        url: 'https://raw.githubusercontent.com/babygrenade/ergo-tokenautics/master/token_list.csv',
         dataType: 'text',
         success: function (data) {
             processData(data, selectedTokenId, selectedCommitId);
